78567a2db4e6a94b31be79e46e92fc52f8a0178a
mayankk-1804 <mayankmathur1804@gmail.com>
2026-02-10 12:28:29 +0530

    first commit

---
+++ 02variables/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "Mayank Mathur"
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T \n", username)
+	
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
+	
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("Variable is of type: %T \n", smallVal)
+
+	var smallFloat float64 = 255.5687326547843678563
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable is of type: %T \n", smallFloat)
+
+	// default values and some aliases
+
+	var anotherVariable int
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable is of type: %T \n", anotherVariable)
+
+	var anotherStringVariable string
+	fmt.Println(anotherStringVariable)
+	fmt.Printf("Variable is of type: %T \n", anotherStringVariable)
+
+
+	//implicit way of declaring a variable
+
+	var website = "google.com"
+	fmt.Println(website)
+}
